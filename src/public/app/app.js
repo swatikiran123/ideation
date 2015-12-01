b64feb230b8937b2bfc41d625adf94f15c0e821f
sankarvema <sans.vema@gmail.com>
2015-12-01 14:28:03 +0530

    dormant idea references removed

--- src/public/app/app.js
+++ src/public/app/app.js
@@ -2,6 +2,6 @@
 
 angular.module('todo-app', ['todo']);
 angular.module('campaign-app', ['campaignModule']);
-angular.module('idea-app', ['ideaModule']);
+//angular.module('idea-app', ['ideaModule']);
 angular.module('user-app', ['userModule']);
 angular.module('login-app', ['loginModule']);
