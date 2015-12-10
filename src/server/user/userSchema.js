063063194284b9adeaf3a976ad4ebbcc38bdfa6a
sankarvema <sans.vema@gmail.com>
2015-12-10 22:47:07 +0530

    User grid functionality ready, styling left

--- src/server/user/userSchema.js
+++ src/server/user/userSchema.js
@@ -7,16 +7,16 @@ var userSchema = new mongoose.Schema({
   firstname		: { type: String, required: true, trim: true },
   lastname		: { type: String, required: true, trim: true },
   contactNo		: { type: String, required: false, trim: true },
-  pwdHash		: { type: String, required: true, trim: true },
-  createdOn		: { type: Date, required: true },
-  lastLogin		: { type: String, required: false, trim: true },
+  pwdHash		: { type: String, required: true, trim: true, default: 'abcd' },
+  createdOn		: { type: Date, required: true, default: Date.now },
+  lastLogin		: { type: Date, required: false, default: Date.now },
 
   // statusFlag enum to represent account status
   //   Unconfirmed: User created but email confirmation not complete
   //   Active: User active for regular operations
   //   Locked: User locked by admin or unsuccessful login attempts
   //   Inactive: Deactivated due to certain reasons like not used for long time
-  statusFlag	: { type: String, required: true } 					
+  statusFlag	: { type: String, required: true, default: 'registered' } 					
 });
 
 module.exports = mongoose.model('users', userSchema);
