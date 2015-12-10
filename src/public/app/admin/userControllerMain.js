10c7ff5ff8b0a13e6e90f6276da1521a157580e3
sankarvema <sans.vema@gmail.com>
2015-12-10 22:47:07 +0530

    User grid functionality ready, styling left

--- src/public/app/admin/userControllerMain.js
+++ src/public/app/admin/userControllerMain.js
@@ -1,6 +1,77 @@
 'use strict';
 
 angular.module('ideation.admin')
-  .controller('userControllerMain', ['$scope', function ($scope) {
+  .controller('userControllerMain', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.hideFilter = true;
+    $scope.hideAddRow = true;
+    $scope.action = "none";
+
+    var refresh = function() {
+      $http.get('/userApi').success(function(response) {
+        console.log("I got the data I requested");
+        $scope.userlist = response;
+        $scope.user = "";
+      });
+    };
+
+    refresh();
+
+    $scope.addRecord = function(){
+      $scope.hideAddRow = false;
+      $scope.action = "add";
+    };
+
+    $scope.save = function() {
+      switch($scope.action)
+      {
+        case "add":
+          $scope.addUser();
+          break;
+
+        case "edit":
+          $scope.update();
+          break;
+      }
+    }
+
+    $scope.addUser = function() {
+      console.log($scope.user);
+      $http.post('/userApi', $scope.user).success(function(response) {
+        console.log(response);
+        refresh();
+        $scope.action = "none";
+        $scope.hideAddRow = true;
+      });
+    };
+
+    $scope.remove = function(id) {
+      console.log(id);
+      $http.delete('/userApi/' + id).success(function(response) {
+        refresh();
+      });
+    };
+
+    $scope.edit = function(id) {
+      console.log(id);
+      $http.get('/userApi/' + id).success(function(response) {
+        $scope.user = response;
+        $scope.hideAddRow = false;
+        $scope.action = "edit";
+      });
+    }; 
+
+    $scope.update = function() {
+      console.log($scope.user._id);
+      $http.put('/userApi/' + $scope.user._id, $scope.user).success(function(response) {
+        refresh();
+        $scope.action = "none";
+        $scope.hideAddRow = true;
+      })
+    };
+
+    $scope.deselect = function() {
+      $scope.user = "";
+    }
 
   }]);
