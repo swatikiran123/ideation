ce857df0208f7518d3e3a68025eabd4ae2c2fcff
sankarvema <sans.vema@gmail.com>
2015-12-11 21:55:53 +0530

    Notification setting done

--- src/public/app/main/mainModule.js
+++ src/public/app/main/mainModule.js
@@ -43,5 +43,12 @@ angular.module('ideation.main', [
 	  	redirectTo: '/'
 	  });
 */	  
-}]);
+}])
+
+.config(['growlProvider', function(growlProvider) {
+	growlProvider.globalReversedOrder(true);
+	growlProvider.globalTimeToLive({success: 1000, error: 2000, warning: 3000, info: 4000});
+	growlProvider.globalDisableCountDown(true);
+	growlProvider.globalPosition('top-center');
+}]);;
 
